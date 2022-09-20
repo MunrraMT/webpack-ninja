c1de78fc72287e3d983dae4ca94b9c92a7ccf29f
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-09-19 23:29:31 -0400

    Aula - 7. Basic HTML Javascript Setup

---
+++ secao-01/1-getting-started/src/index.js
@@ -0,0 +1,13 @@
+function buttonClicked() {
+  const el = document.querySelector('#header');
+  el.innerHTML = 'Hey i have updated the code!';
+
+  const listItem = ['apple', 'orange', 'banana'];
+  const ul = document.querySelector('#shopping-list');
+
+  _.forEach(listItem, function (item) {
+    const tempEl = document.createElement('li');
+    tempEl.innerHTML = item;
+    ul.appendChild(tempEl);
+  });
+}
