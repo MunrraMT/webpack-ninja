b22982f92b883ee8b987eabb23ce5ab496bdc22d
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-09-21 23:40:46 -0400

    aula - 15. CSS loaders Part 2

--- secao-02/3-css-loaders/src/index.js
+++ secao-02/3-css-loaders/src/index.js
@@ -1,16 +1,30 @@
-import './index.css';
 // import _ from 'lodash';
+
+import style from './index.css';
+import './clearButton';
+
+console.log(style);
 
 function buttonClicked() {
   const el = document.querySelector('#header');
   el.innerHTML = 'Hey i have updated the code!';
   const listItem = ['apple', 'orange', 'banana'];
   const ul = document.querySelector('#shopping-list');
-  _.forEach(listItem, function (item) {
+
+  listItem.forEach((item) => {
     const tempEl = document.createElement('li');
     tempEl.innerHTML = item;
     ul.appendChild(tempEl);
   });
+
+  // _.forEach(listItem, function (item) {
+  //   const tempEl = document.createElement('li');
+  //   tempEl.innerHTML = item;
+  //   ul.appendChild(tempEl);
+  // });
 }
 
-document.querySelector('#button-1').addEventListener('click', buttonClicked);
+const btn1 = document.querySelector('#button-1');
+btn1.addEventListener('click', buttonClicked);
+// btn1.classList.add([style.button]);
+btn1.classList.add(['button']);
