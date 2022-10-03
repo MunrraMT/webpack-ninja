a525c9ca39043f1aabaf77cbc77a9396a565c04f
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-03 15:30:18 -0400

    Aula - 51. Adding Loaders | Dev server configurations

--- secao-07/10.1 React_project_BasicSetup/webpack.config.js
+++ secao-07/10.1 React_project_BasicSetup/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: '[name].bundle.js',
@@ -13,4 +15,31 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      port: 9000,
+      open: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif|svg)$/,
+        use: 'asset/resource',
+      },
+    ],
+  },
 };
