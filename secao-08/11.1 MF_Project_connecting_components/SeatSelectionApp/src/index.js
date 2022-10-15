1e283f445ade61c9db1dd6c00374aac9d4cf7e2c
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-14 23:54:40 -0400

    Aula - 66. Initialise more Micro-frontends

--- secao-08/11.1 MF_Project_connecting_components/SeatSelectionApp/src/index.js
+++ secao-08/11.1 MF_Project_connecting_components/SeatSelectionApp/src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("app")
+  document.getElementById('app'),
 );
